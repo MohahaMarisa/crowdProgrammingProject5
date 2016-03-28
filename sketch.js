e8061374352456061409ded99730cbb86f3606c4
MohahaMarisa <contactmarisalu@gmail.com>
2016-03-28 00:30:13 -0400

    RENAMED DAMAGE and game lost

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@
 //these are all the variables dealing with creating the force vector that appears
 //when the user clicks and drags their mouse
 var howLongDidTheySurvive;
+var didTheyLose;
 var forceBegin;
 var mouseXForceVector;
 var mouseYForceVector;
@@ -83,7 +84,10 @@ function setup() {
 function draw(){
   background(0,100,119);
   textSize(24);
-  text("Amount of Fluffy Cloud Wool: "+ numCloudsCollected, 30,30);
+  text("Amount of damage: "+ numCloudsCollected, 30,30);
+  if (didTheyLose=true){
+    text("GAME LOST", 30, 60);
+  }
   noStroke();
     for(var i=0; i<100; i++){
     fill(237,241,210, i/100*255);
@@ -214,6 +218,7 @@ function UglySheep(){
             isItFluffy = false;
             numCloudsCollected = 0;
             sheepMass = 15;
+            didTheyLose = true;
 
             //initial position
             this.x = width/2;
