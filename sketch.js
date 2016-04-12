0b3d1ad6a33168da34f66f930813f9b91e390d2f
MohahaMarisa <contactmarisalu@gmail.com>
2016-04-12 00:01:37 -0400

    changed shiz up

--- sketch.js
+++ sketch.js
@@ -251,41 +251,41 @@ function UglySheep(){
  *
  **/
 
-// selector used by jquery to identify your form
-var form_selector = "#mturk_form";
+// // selector used by jquery to identify your form
+// var form_selector = "#mturk_form";
 
-// function for getting URL parameters
-function gup(name) {
-  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-  var regexS = "[\\?&]"+name+"=([^&#]*)";
-  var regex = new RegExp(regexS);
-  var results = regex.exec(window.location.href);
-  if(results == null)
-    return "";
-  else return unescape(results[1]);
-}
+// // function for getting URL parameters
+// function gup(name) {
+//   name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+//   var regexS = "[\\?&]"+name+"=([^&#]*)";
+//   var regex = new RegExp(regexS);
+//   var results = regex.exec(window.location.href);
+//   if(results == null)
+//     return "";
+//   else return unescape(results[1]);
+// }
 
-//  Turkify the captioning page.
-$(document).ready(function () {
-  // is assigntmentId is a URL parameter
-  if((aid = gup("assignmentId"))!="" && $(form_selector).length>0) {
+// //  Turkify the captioning page.
+// $(document).ready(function () {
+//   // is assigntmentId is a URL parameter
+//   if((aid = gup("assignmentId"))!="" && $(form_selector).length>0) {
 
-    // If the HIT hasn't been accepted yet, disabled the form fields.
-    if(aid == "ASSIGNMENT_ID_NOT_AVAILABLE") {
-      $('input,textarea,select').attr("DISABLED", "disabled");
-    }
+//     // If the HIT hasn't been accepted yet, disabled the form fields.
+//     if(aid == "ASSIGNMENT_ID_NOT_AVAILABLE") {
+//       $('input,textarea,select').attr("DISABLED", "disabled");
+//     }
 
-    // Add a new hidden input element with name="assignmentId" that
-    // with assignmentId as its value.
-    var aid_input = $("<input type='hidden' name='assignmentId' value='" + aid + "'>").appendTo($(form_selector));
-    // var aid_input = $("<input type='hidden' name='timeLived' value='" + foooklation of alltime variableforhowlongtheylivedinmycode+ "'>").appendTo($(form_selector));
+//     // Add a new hidden input element with name="assignmentId" that
+//     // with assignmentId as its value.
+//     var aid_input = $("<input type='hidden' name='assignmentId' value='" + aid + "'>").appendTo($(form_selector));
+//     //var aid_input = $("<input type='hidden' name='timeLived' value='" + foooklation of alltime variableforhowlongtheylivedinmycode+ "'>").appendTo($(form_selector));
 
-    // Make sure the submit form's method is POST
-    $(form_selector).attr('method', 'POST');
+//     // Make sure the submit form's method is POST
+//     $(form_selector).attr('method', 'POST');
 
-    // Set the Action of the form to the provided "turkSubmitTo" field
-    if((submit_url=gup("turkSubmitTo"))!="") {
-      $(form_selector).attr('action', submit_url + '/mturk/externalSubmit');
-    }
-  }
-});
+//     // Set the Action of the form to the provided "turkSubmitTo" field
+//     if((submit_url=gup("turkSubmitTo"))!="") {
+//       $(form_selector).attr('action', submit_url + '/mturk/externalSubmit');
+//     }
+//   }
+// });
